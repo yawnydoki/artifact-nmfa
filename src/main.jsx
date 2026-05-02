597724e3d38d1f224e8ea55109d3c1ee757c4ed4
larr <luceslarrion@gmail.com>
2026-05-02 20:30:14 +0800

    fixed: 'stopProcessVideo' error

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import './index.css'
 import { LanguageProvider } from './LanguageContext.jsx' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <LanguageProvider>  
-      <App />
-    </LanguageProvider>
-  </React.StrictMode>,
+  <LanguageProvider>  
+    <App />
+  </LanguageProvider>
 )
