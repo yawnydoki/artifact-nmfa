9a7fa8cf699fe6e523dcccc60a83aed08772b4e7
larr <luceslarrion@gmail.com>
2026-06-10 16:32:12 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -49,13 +49,13 @@ export default defineConfig({
         orientation: "portrait",
         icons: [
           {
-            src: "/logo_trans.png",
+            src: "/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any maskable",
           },
           {
-            src: "/logo_trans.png",
+            src: "/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
